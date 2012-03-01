69721d41e2911e3461009a0857a2e372d31ce173
Alessandro Arnodo <a.arnodo@gmail.com>
2012-03-01 01:08:50 +0100

    fixed form validation class

--- libraries/MY_Form_validation.php
+++ libraries/MY_Form_validation.php
@@ -25,6 +25,7 @@ class MY_Form_validation extends CI_Form_validation{
     public function __construct($rules = array())
     {
         log_message('debug', '*** Hello from MY_Session ***');
+        $this->CI =& get_instance();
         $this->CI->load->library("cimongo/cimongo");
         parent::__construct($rules);
     }
